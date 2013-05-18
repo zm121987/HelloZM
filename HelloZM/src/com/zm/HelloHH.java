97394d7a583a6002fb4797bc2d377404605d39e2
zm121987 <zm121987@163.com>
2013-05-18 15:01:03 +0800

    first

---
+++ HelloZM/src/com/zm/HelloHH.java
@@ -0,0 +1,32 @@
+package com.zm;
+
+public class HelloHH {
+
+	/**
+	 * @param args
+	 */
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
