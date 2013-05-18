707cd15600e6d5cffeff6f3936530c3af189012d
zm121987 <zm121987@163.com>
2013-05-18 16:07:30 +0800

    HelloFF

---
+++ HelloZM/src/com/zm/HelloFF.java
@@ -0,0 +1,27 @@
+package com.zm;
+
+public class HelloFF {
+
+	/**
+	 * @param args
+	 */
+	int i;
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	int j;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
